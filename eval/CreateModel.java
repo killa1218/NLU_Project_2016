2f4de723d970fea9efcfa97c45d356c9ec33f75b
staceytay <stay.jw@gmail.com>
2014-09-02 15:00:31 +0800

    Re-indent and whitespace cleanup

--- eval/CreateModel.java
+++ eval/CreateModel.java
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-//////////////////////////////////////////////////////////////////////////////   
+//////////////////////////////////////////////////////////////////////////////
 
 import java.io.File;
 import java.io.FileReader;
@@ -38,72 +38,72 @@ import opennlp.maxent.io.SuffixSensitiveGISModelWriter;
  */
 public class CreateModel {
 
-    // some parameters if you want to play around with the smoothing option
-    // for model training.  This can improve model accuracy, though training
-    // will potentially take longer and use more memory.  Model size will also
-    // be larger.  Initial testing indicates improvements for models built on
-    // small data sets and few outcomes, but performance degradation for those
-    // with large data sets and lots of outcomes.
-    public static boolean USE_SMOOTHING = false;
-    public static double SMOOTHING_OBSERVATION = 0.1;
-    
-    private static void usage() {
-      System.err.println("java CreateModel [-real] dataFile");
-      System.exit(1);
-    }
-    
-    /**
-     * Main method. Call as follows:
-     * <p>
-     * java CreateModel dataFile
-     */
-    public static void main (String[] args) {
-      int ai = 0;
-      boolean real = false;
-      while (args[ai].startsWith("-")) {
-        if (args[ai].equals("-real")) {
-          real = true;
-        }
-        else {
-          System.err.println("Unknown option: "+args[ai]);
-          usage();
-        }
-        ai++;
+  // some parameters if you want to play around with the smoothing option
+  // for model training.  This can improve model accuracy, though training
+  // will potentially take longer and use more memory.  Model size will also
+  // be larger.  Initial testing indicates improvements for models built on
+  // small data sets and few outcomes, but performance degradation for those
+  // with large data sets and lots of outcomes.
+  public static boolean USE_SMOOTHING = false;
+  public static double SMOOTHING_OBSERVATION = 0.1;
+
+  private static void usage() {
+    System.err.println("java CreateModel [-real] dataFile");
+    System.exit(1);
+  }
+
+  /**
+   * Main method. Call as follows:
+   * <p>
+   * java CreateModel dataFile
+   */
+  public static void main (String[] args) {
+    int ai = 0;
+    boolean real = false;
+    while (args[ai].startsWith("-")) {
+      if (args[ai].equals("-real")) {
+        real = true;
       }
-      String dataFileName = new String(args[ai]);
-      //String modelFileName =
-      //  dataFileName.substring(0,dataFileName.lastIndexOf('.'))
-      //  + "Model.txt";
-      String modelFileName =
-        dataFileName.substring(0,dataFileName.lastIndexOf('.'))
-        + ".model";
-      try {
-        FileReader datafr = new FileReader(new File(dataFileName));
-        EventStream es;
-        if (!real) { 
-          es = new BasicEventStream(new PlainTextByLineDataStream(datafr));
-        }
-        else {
-          es = new RealBasicEventStream(new PlainTextByLineDataStream(datafr));
-        }
-        GIS.SMOOTHING_OBSERVATION = SMOOTHING_OBSERVATION;
-        GISModel model;
-        if (!real) {
-          model = GIS.trainModel(es,USE_SMOOTHING);
-        }
-        else {
-          model = GIS.trainModel(100, new OnePassRealValueDataIndexer(es,0), USE_SMOOTHING);
-        }
-        
-        File outputFile = new File(modelFileName);
-        GISModelWriter writer =
-          new SuffixSensitiveGISModelWriter(model, outputFile);
-        writer.persist();
-      } catch (Exception e) {
-        System.out.print("Unable to create model due to exception: ");
-        System.out.println(e);
-        e.printStackTrace();
+      else {
+        System.err.println("Unknown option: "+args[ai]);
+        usage();
       }
+      ai++;
     }
+    String dataFileName = new String(args[ai]);
+    //String modelFileName =
+    //  dataFileName.substring(0,dataFileName.lastIndexOf('.'))
+    //  + "Model.txt";
+    String modelFileName =
+      dataFileName.substring(0,dataFileName.lastIndexOf('.'))
+      + ".model";
+    try {
+      FileReader datafr = new FileReader(new File(dataFileName));
+      EventStream es;
+      if (!real) {
+        es = new BasicEventStream(new PlainTextByLineDataStream(datafr));
+      }
+      else {
+        es = new RealBasicEventStream(new PlainTextByLineDataStream(datafr));
+      }
+      GIS.SMOOTHING_OBSERVATION = SMOOTHING_OBSERVATION;
+      GISModel model;
+      if (!real) {
+        model = GIS.trainModel(es,USE_SMOOTHING);
+      }
+      else {
+        model = GIS.trainModel(100, new OnePassRealValueDataIndexer(es,0), USE_SMOOTHING);
+      }
+
+      File outputFile = new File(modelFileName);
+      GISModelWriter writer =
+        new SuffixSensitiveGISModelWriter(model, outputFile);
+      writer.persist();
+    } catch (Exception e) {
+      System.out.print("Unable to create model due to exception: ");
+      System.out.println(e);
+      e.printStackTrace();
+    }
+  }
 
 }
